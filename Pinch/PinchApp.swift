bdb215bfe0539a342de8fc18b937fdf248b1fb87
jozek-hajduk_globant <jozek.hajduk@globant.com>
2024-09-12 17:06:37 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jozek Andrzej Hajduk Sanchez on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
