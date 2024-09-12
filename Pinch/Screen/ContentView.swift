30687c0a7215c65122ea4f4951c15948a97981a9
HajdukSanchez <hajduksanchez.dev@gmail.com>
2024-09-12 18:15:23 -0500

    add drag image value

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,17 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Properties
+    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Image("magazine-front-cover")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale < 5 {
+                            withAnimation(.spring) {
+                                imageScale += 2
+                            }
+                        } else {
+                            withAnimation(.spring) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+                    .onTapGesture {
+                        if imageScale > 1 {
+                            withAnimation(.spring) {
+                                imageScale = 1
+                            }
+                        }
+                    }
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
