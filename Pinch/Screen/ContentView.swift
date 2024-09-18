23f6b430ba69706290679fcd8d0c3e8d77f4a8be
HajdukSanchez <hajduksanchez.dev@gmail.com>
2024-09-18 18:00:10 -0500

    add drawer

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1.0
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = true
     
     // MARK: - Functions
     func resetImageState() {
@@ -129,6 +130,29 @@ struct ContentView: View {
                     .opacity(isAnimating ? 1 : 0)
                 }
                 .padding(.bottom, 30)
+            }
+            .overlay(alignment: .topTrailing) {
+                HStack(spacing: 12) {
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundStyle(.secondary)
+                        .onTapGesture {
+                            withAnimation(.easeOut) {
+                                isDrawerOpen.toggle()
+                            }
+                        }
+                    Spacer()
+                }
+                .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .opacity(isAnimating ? 1 : 0)
+                .frame(width: 260)
+                .padding(.top, UIScreen.main.bounds.height / 12)
+                .offset(x: isDrawerOpen ? 20 : 215)
             }
         }
     }
